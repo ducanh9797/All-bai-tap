5d967a8368a72e04ffda7d46e8251ae67d627f48
KING JAME <ducanhdovts@gmail.com>
2018-06-14 09:22:53 +0700

    thực hành kiểm tra năm nhuận

--- KiemtraNamNhuan.java
+++ KiemtraNamNhuan.java
@@ -19,5 +19,25 @@ public class KiemtraNamNhuan {
         } else {
             System.out.printf("%d is NOT a leap year", year);
         }
+        boolean isLeapYear = false;
+
+        boolean isDivisibleBy4 = year % 4 == 0;
+        if(isDivisibleBy4){
+            boolean isDivisibleBy100 = year % 100 == 0;
+            if(isDivisibleBy100){
+                boolean isDivisibleBy400 = year % 400 == 0;
+                if(isDivisibleBy400){
+                    isLeapYear = true;
+                }
+            } else {
+                isLeapYear = true;
+            }
+        }
+
+        if(isLeapYear){
+            System.out.printf("%d is a leap year", year);
+        } else {
+            System.out.printf("%d is NOT a leap year", year);
+        }
     }
 }
